597ba2bb5ef64bf2594fb3065e6d2dcd29c6792f
bt4it <bt4it2@gmail.com>
2026-07-14 19:01:58 +0200

    minor update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   default_tags {
     tags = {
       Environment  = "Test-bt STG"
-      Tagged-to-be = "Example-tags bt STG"
+      Tagged-to-be = "Example-tags bt STG ... "
     }
   }
 }
